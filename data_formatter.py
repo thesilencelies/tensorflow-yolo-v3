c5097ffcaa5479742205607ce75b8bbd521bc160
Stephen Lilico <gonetothewind@gmail.com>
2018-10-20 18:09:40 +0900

    created dummy files to edit from mobile devices

---
+++ data_formatter.py
@@ -0,0 +1,9 @@
+""" pasrses the face detection dataset into a format that tensorflow can use """
+
+import tensorflow as tf
+import json
+
+#TODO write these functions
+
+
+#TODO add data augmentation
---
+++ yolov3_face_train.py
@@ -0,0 +1,3 @@
+""" script to fine tune a loaded yolov3 network for face detection """
+
+#TODO
